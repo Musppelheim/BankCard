05d6f71a11cf2ce7e45f7ecf2b6685fcbc314c8b
Musppelheim <neloliver@hotmail.com>
2020-06-15 00:31:59 +0700

    Troubles with driver fixed

--- src/test/java/ru/netology/domain/CardApp.java
+++ src/test/java/ru/netology/domain/CardApp.java
@@ -1,5 +1,6 @@
 package ru.netology.domain;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -7,23 +8,25 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
-
+import org.openqa.selenium.firefox.FirefoxOptions;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 public class CardApp {
     private WebDriver driver;
 
     @BeforeAll
     static void setUpAll() {
-        System.setProperty("webdriver.gecko.driver.", "path/to/geckodriver.exe");
-        DesiredCapabilities capabilities = DesiredCapabilities.firefox();
-        capabilities.setCapability("marionette", true);
+        WebDriverManager.firefoxdriver().setup();
     }
+
     @BeforeEach
-    void setUp() {
-        driver = new FirefoxDriver();
-        driver.get("https://github.com/mozilla/geckodriver");
+    void setUpTest() {
+        FirefoxOptions options = new FirefoxOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new FirefoxDriver(options);
     }
 
     @AfterEach
@@ -35,12 +38,33 @@ public class CardApp {
     @Test
     void ShouldSubmit() {
         driver.get("http://localhost:9999");
-        driver.findElement(By.cssSelector("input[type='text']")).sendKeys("Лоран Сика");
-        driver.findElement(By.cssSelector("input[type='tel']")).sendKeys("+79526007441");
-        driver.findElement(By.cssSelector(".checkbox_box")).click();
-        driver.findElement(By.cssSelector("span[class='button_text']")).submit();
-        String successText = driver.findElement(By.cssSelector("[data-test-id]")).getText();
-        assertEquals("Ваша заявка успешно отправлена!  Наш менеджер свяжется с вами в ближайшее время.", successText);
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Лоран Сика");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input ")).sendKeys("+79526007441");
+        driver.findElement(By.cssSelector("span[class='checkbox__box']")).click();
+        driver.findElement(By.cssSelector(".button__text")).submit();
+        String text = driver.findElement(By.cssSelector("[data-test-id]")).getText();
+        assertEquals("  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text);
+    }
 
+    @Test
+    void souldNotSubmitWithWrongName() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Лоран сика");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input ")).sendKeys("+79526007441");
+        driver.findElement(By.cssSelector("span[class='checkbox__box']")).click();
+        driver.findElement(By.cssSelector(".button__text")).submit();
+        String text = driver.findElement(By.cssSelector(".input__sub")).getText();
+        assertEquals("Укажите точно как в паспорте", text);
+    }
+
+    @Test
+    void souldNotSubmitWithEmptyNameField() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input ")).sendKeys("+79526007441");
+        driver.findElement(By.cssSelector("span[class='checkbox__box']")).click();
+        driver.findElement(By.cssSelector(".button__text")).submit();
+        String text = driver.findElement(By.cssSelector(".input__sub")).getText();
+        assertEquals("Поле обязательно для заполнения", text);
     }
 }
